6dcd557263b31af2e973aee92ae101511728b8ff
stephen51 <istephenisac@gmail.com>
2022-09-05 16:55:39 +0530

    first commit

---
+++ src/main/java/com/stephencode/githubactionsgke/TestController.java
@@ -0,0 +1,13 @@
+package com.stephencode.githubactionsgke;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "welcome to spring boot";
+    }
+}
